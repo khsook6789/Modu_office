742c1d7250f69362a205bdc3683d39e04b71460b
khsook6789 <khsook8995@gmail.com>
2026-01-31 01:34:18 +0900

    feat: Oauth2 관련 column 추가, password_hash -> nullable로 변경

---
+++ backend/src/main/java/com/modu/office/entity/enums/LoginType.java
@@ -0,0 +1,12 @@
+package com.modu.office.entity.enums;
+
+/**
+ * 로그인 유형
+ * - LOCAL: 일반 이메일/비밀번호 로그인
+ * - NAVER: 네이버 OAuth2 로그인
+ * - google도 추가 예정
+ */
+public enum LoginType {
+    LOCAL,
+    NAVER
+}
